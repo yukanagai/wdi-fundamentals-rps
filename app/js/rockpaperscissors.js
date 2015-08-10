1f72545fa6c5ec433477899da1b85c5c256c37c8
Yuka Nagai <yukanagai@gmail.com>
2015-08-10 06:11:59 -0400

    Update rockpaperscissors.js

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,6 +22,8 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
+    var move = x
+    if (move = null) 
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
